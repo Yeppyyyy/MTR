7cf49126db32b62b50d10da9cdf73f8759270c44
du-yuwen <1693286846@qq.com>
2024-08-25 12:00:16 +0800

    data processing

--- mtr/datasets/waymo/data_preprocess.py
+++ mtr/datasets/waymo/data_preprocess.py
@@ -1,6 +1,6 @@
 # Motion Transformer (MTR): https://arxiv.org/abs/2209.13508
 # Published at NeurIPS 2022
-# Written by Shaoshuai Shi 
+# Written by Shaoshuai Shi
 # All Rights Reserved
 
 
@@ -10,11 +10,12 @@ import pickle
 import tensorflow as tf
 import multiprocessing
 import glob
+import shutil
 from tqdm import tqdm
 from waymo_open_dataset.protos import scenario_pb2
 from waymo_types import object_type, lane_type, road_line_type, road_edge_type, signal_state, polyline_type
 
-    
+
 def decode_tracks_from_proto(tracks):
     track_infos = {
         'object_id': [],  # {0: unset, 1: vehicle, 2: pedestrian, 3: cyclist, 4: others}
@@ -59,27 +60,29 @@ def decode_map_features_from_proto(map_features):
 
         if cur_data.lane.ByteSize() > 0:
             cur_info['speed_limit_mph'] = cur_data.lane.speed_limit_mph
-            cur_info['type'] = lane_type[cur_data.lane.type]  # 0: undefined, 1: freeway, 2: surface_street, 3: bike_lane
+            cur_info['type'] = lane_type[
+                cur_data.lane.type]  # 0: undefined, 1: freeway, 2: surface_street, 3: bike_lane
 
             cur_info['interpolating'] = cur_data.lane.interpolating
             cur_info['entry_lanes'] = list(cur_data.lane.entry_lanes)
             cur_info['exit_lanes'] = list(cur_data.lane.exit_lanes)
 
             cur_info['left_boundary'] = [{
-                    'start_index': x.lane_start_index, 'end_index': x.lane_end_index,
-                    'feature_id': x.boundary_feature_id,
-                    'boundary_type': x.boundary_type  # roadline type
-                } for x in cur_data.lane.left_boundaries
+                'start_index': x.lane_start_index, 'end_index': x.lane_end_index,
+                'feature_id': x.boundary_feature_id,
+                'boundary_type': x.boundary_type  # roadline type
+            } for x in cur_data.lane.left_boundaries
             ]
             cur_info['right_boundary'] = [{
-                    'start_index': x.lane_start_index, 'end_index': x.lane_end_index,
-                    'feature_id': x.boundary_feature_id,
-                    'boundary_type': road_line_type[x.boundary_type]  # roadline type
-                } for x in cur_data.lane.right_boundaries
+                'start_index': x.lane_start_index, 'end_index': x.lane_end_index,
+                'feature_id': x.boundary_feature_id,
+                'boundary_type': road_line_type[x.boundary_type]  # roadline type
+            } for x in cur_data.lane.right_boundaries
             ]
 
             global_type = polyline_type[cur_info['type']]
-            cur_polyline = np.stack([np.array([point.x, point.y, point.z, global_type]) for point in cur_data.lane.polyline], axis=0)
+            cur_polyline = np.stack(
+                [np.array([point.x, point.y, point.z, global_type]) for point in cur_data.lane.polyline], axis=0)
             cur_polyline_dir = get_polyline_dir(cur_polyline[:, 0:3])
             cur_polyline = np.concatenate((cur_polyline[:, 0:3], cur_polyline_dir, cur_polyline[:, 3:]), axis=-1)
 
@@ -89,7 +92,8 @@ def decode_map_features_from_proto(map_features):
             cur_info['type'] = road_line_type[cur_data.road_line.type]
 
             global_type = polyline_type[cur_info['type']]
-            cur_polyline = np.stack([np.array([point.x, point.y, point.z, global_type]) for point in cur_data.road_line.polyline], axis=0)
+            cur_polyline = np.stack(
+                [np.array([point.x, point.y, point.z, global_type]) for point in cur_data.road_line.polyline], axis=0)
             cur_polyline_dir = get_polyline_dir(cur_polyline[:, 0:3])
             cur_polyline = np.concatenate((cur_polyline[:, 0:3], cur_polyline_dir, cur_polyline[:, 3:]), axis=-1)
 
@@ -99,7 +103,8 @@ def decode_map_features_from_proto(map_features):
             cur_info['type'] = road_edge_type[cur_data.road_edge.type]
 
             global_type = polyline_type[cur_info['type']]
-            cur_polyline = np.stack([np.array([point.x, point.y, point.z, global_type]) for point in cur_data.road_edge.polyline], axis=0)
+            cur_polyline = np.stack(
+                [np.array([point.x, point.y, point.z, global_type]) for point in cur_data.road_edge.polyline], axis=0)
             cur_polyline_dir = get_polyline_dir(cur_polyline[:, 0:3])
             cur_polyline = np.concatenate((cur_polyline[:, 0:3], cur_polyline_dir, cur_polyline[:, 3:]), axis=-1)
 
@@ -116,7 +121,8 @@ def decode_map_features_from_proto(map_features):
             map_infos['stop_sign'].append(cur_info)
         elif cur_data.crosswalk.ByteSize() > 0:
             global_type = polyline_type['TYPE_CROSSWALK']
-            cur_polyline = np.stack([np.array([point.x, point.y, point.z, global_type]) for point in cur_data.crosswalk.polygon], axis=0)
+            cur_polyline = np.stack(
+                [np.array([point.x, point.y, point.z, global_type]) for point in cur_data.crosswalk.polygon], axis=0)
             cur_polyline_dir = get_polyline_dir(cur_polyline[:, 0:3])
             cur_polyline = np.concatenate((cur_polyline[:, 0:3], cur_polyline_dir, cur_polyline[:, 3:]), axis=-1)
 
@@ -124,15 +130,15 @@ def decode_map_features_from_proto(map_features):
 
         elif cur_data.speed_bump.ByteSize() > 0:
             global_type = polyline_type['TYPE_SPEED_BUMP']
-            cur_polyline = np.stack([np.array([point.x, point.y, point.z, global_type]) for point in cur_data.speed_bump.polygon], axis=0)
+            cur_polyline = np.stack(
+                [np.array([point.x, point.y, point.z, global_type]) for point in cur_data.speed_bump.polygon], axis=0)
             cur_polyline_dir = get_polyline_dir(cur_polyline[:, 0:3])
             cur_polyline = np.concatenate((cur_polyline[:, 0:3], cur_polyline_dir, cur_polyline[:, 3:]), axis=-1)
 
             map_infos['speed_bump'].append(cur_info)
 
         else:
-            print(cur_data)
-            raise ValueError
+            pass
 
         polylines.append(cur_polyline)
         cur_info['polyline_index'] = (point_cnt, point_cnt + len(cur_polyline))
@@ -167,91 +173,103 @@ def decode_dynamic_map_states_from_proto(dynamic_map_states):
     return dynamic_map_infos
 
 
-def process_waymo_data_with_scenario_proto(data_file, output_path=None):
-    dataset = tf.data.TFRecordDataset(data_file, compression_type='')
+def parse_data(input_path, output_path, pre_fix=None, max_files=None):
+    cnt = 0
+    MAX = 100000
     ret_infos = []
-    for cnt, data in enumerate(dataset):
-        info = {}
-        scenario = scenario_pb2.Scenario()
-        scenario.ParseFromString(bytearray(data.numpy()))
+    file_list = os.listdir(input_path)
 
-        info['scenario_id'] = scenario.scenario_id
-        info['timestamps_seconds'] = list(scenario.timestamps_seconds)  # list of int of shape (91)
-        info['current_time_index'] = scenario.current_time_index  # int, 10
-        info['sdc_track_index'] = scenario.sdc_track_index  # int
-        info['objects_of_interest'] = list(scenario.objects_of_interest)  # list, could be empty list
+    #file_list = [f for f in file_list if f.endswith('tfrecord')]
 
-        info['tracks_to_predict'] = {
-            'track_index': [cur_pred.track_index for cur_pred in scenario.tracks_to_predict],
-            'difficulty': [cur_pred.difficulty for cur_pred in scenario.tracks_to_predict]
-        }  # for training: suggestion of objects to train on, for val/test: need to be predicted
+    if max_files:
+        file_list = file_list[:max_files]
 
-        track_infos = decode_tracks_from_proto(scenario.tracks)
-        info['tracks_to_predict']['object_type'] = [track_infos['object_type'][cur_idx] for cur_idx in info['tracks_to_predict']['track_index']]
+    for file in tqdm(file_list):
+        file_path = os.path.join(input_path, file)
 
-        # decode map related data
-        map_infos = decode_map_features_from_proto(scenario.map_features)
-        dynamic_map_infos = decode_dynamic_map_states_from_proto(scenario.dynamic_map_states)
+        dataset = tf.data.TFRecordDataset(file_path, compression_type='')
 
-        save_infos = {
-            'track_infos': track_infos,
-            'dynamic_map_infos': dynamic_map_infos,
-            'map_infos': map_infos
-        }
-        save_infos.update(info)
+        for j, data in enumerate(dataset):
+            scenario = scenario_pb2.Scenario()
+            scenario.ParseFromString(bytearray(data.numpy()))
 
-        output_file = os.path.join(output_path, f'sample_{scenario.scenario_id}.pkl')
-        with open(output_file, 'wb') as f:
-            pickle.dump(save_infos, f)
+            info = {}
+            info['scenario_id'] = scenario.scenario_id
+            info['timestamps_seconds'] = list(scenario.timestamps_seconds)
+            info['current_time_index'] = scenario.current_time_index
+            info['sdc_track_index'] = scenario.sdc_track_index
+            info['objects_of_interest'] = list(scenario.objects_of_interest)
+            info['tracks_to_predict'] = {
+                'track_index': [cur_pred.track_index for cur_pred in scenario.tracks_to_predict],
+                'difficulty': [cur_pred.difficulty for cur_pred in scenario.tracks_to_predict]
+            }
 
-        ret_infos.append(info)
+            track_infos = decode_tracks_from_proto(scenario.tracks)
+            info['tracks_to_predict']['object_type'] = [
+                track_infos['object_type'][cur_idx] for cur_idx in info['tracks_to_predict']['track_index']
+            ]
+
+            # Decode map-related data
+            map_infos = decode_map_features_from_proto(scenario.map_features)
+            dynamic_map_infos = decode_dynamic_map_states_from_proto(scenario.dynamic_map_states)
+
+            save_infos = {
+                'track_infos': track_infos,
+                'dynamic_map_infos': dynamic_map_infos,
+                'map_infos': map_infos
+            }
+            save_infos.update(info)
+
+            ret_infos.append(info)
+            
+            if pre_fix is None:
+                p = os.path.join(output_path, '{}.pkl'.format(cnt))
+            else:
+                p = os.path.join(output_path, '{}_{}.pkl'.format(pre_fix, cnt))
+
+            with open(p, 'wb') as f:
+                pickle.dump(save_infos, f)
+
+            cnt += 1  
+
+
+            if cnt >= MAX:
+                return ret_infos
+
     return ret_infos
 
-
-def get_infos_from_protos(data_path, output_path=None, num_workers=8):
-    from functools import partial
-    if output_path is not None:
-        os.makedirs(output_path, exist_ok=True)
-
-    func = partial(
-        process_waymo_data_with_scenario_proto, output_path=output_path
-    )
-
-    src_files = glob.glob(os.path.join(data_path, '*.tfrecord*'))
-    src_files.sort()
-
-    # func(src_files[0])
-    with multiprocessing.Pool(num_workers) as p:
-        data_infos = list(tqdm(p.imap(func, src_files), total=len(src_files)))
-
-    all_infos = [item for infos in data_infos for item in infos]
-    return all_infos
-
-
-def create_infos_from_protos(raw_data_path, output_path, num_workers=16):
-    train_infos = get_infos_from_protos(
-        data_path=os.path.join(raw_data_path, 'training'),
-        output_path=os.path.join(output_path, 'processed_scenarios_training'),
-        num_workers=num_workers
-    )
-    train_filename = os.path.join(output_path, 'processed_scenarios_training_infos.pkl')
-    with open(train_filename, 'wb') as f:
-        pickle.dump(train_infos, f)
-    print('----------------Waymo info train file is saved to %s----------------' % train_filename)
-
-    val_infos = get_infos_from_protos(
-        data_path=os.path.join(raw_data_path, 'validation'),
-        output_path=os.path.join(output_path, 'processed_scenarios_validation'),
-        num_workers=num_workers
-    )
-    val_filename = os.path.join(output_path, 'processed_scenarios_val_infos.pkl')
-    with open(val_filename, 'wb') as f:
-        pickle.dump(val_infos, f)
-    print('----------------Waymo info val file is saved to %s----------------' % val_filename)
-    
-
 if __name__ == '__main__':
-    create_infos_from_protos(
-        raw_data_path=sys.argv[1],
-        output_path=sys.argv[2]
-    )
+    PATH_A = sys.argv[1] 
+    PATH_B = sys.argv[2] 
+    PATH_C = sys.argv[3] 
+    
+    for i in range(10):
+        os.makedirs('{}/{}'.format(PATH_C, i))
+
+   
+    for i in tqdm(range(10)):
+        for j in tqdm(range(100)):
+            src_file = '{}/uncompressed_scenario_training_20s_training_20s.tfrecord-{:05d}-of-01000'.format(PATH_A, j + 100 * i)
+            dest_dir = '{}/{}/'.format(PATH_C, i)
+
+
+            if os.path.exists(src_file):
+                shutil.move(src_file, dest_dir)
+            else:
+                pass
+
+    source_root = PATH_C + '/{}'
+    target_dir = PATH_B
+
+    all_infos = []
+
+
+    for x in range(10):
+        source_dir = source_root.format(x)
+        infos = parse_data(source_dir, os.path.join(target_dir, 'processed_scenarios'), pre_fix=str(x))
+        all_infos.extend(infos)
+
+    filename = os.path.join(target_dir, 'processed_scenarios_infos.pkl')
+    with open(filename, 'wb') as f:
+        pickle.dump(all_infos, f)
+    print('----------------Waymo info train file is saved to %s----------------' % filename)
